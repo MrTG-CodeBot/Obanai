73fdefc9b334f2edc1e514b24ed1edfd4b3c012a
MrTG-CodeBot <148360334+MrTG-CodeBot@users.noreply.github.com>
2023-12-14 08:00:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,28 +14,28 @@ def is_enabled(value, default):
     else: return default
 
 # basic information
-API_ID = int(os.environ.get('API_ID', '8914119'))
-API_HASH = os.environ.get('API_HASH', '652bae601b07c928b811bdb310fdb4b0')
-BOT_TOKEN = os.environ.get('BOT_TOKEN', '6645084082:AAEvdcfTZ0uFS_Gu6KvVPVBMNlrlWiESPiM')
+API_ID = int(os.environ.get('API_ID', ''))
+API_HASH = os.environ.get('API_HASH', '')
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '')
 PORT = os.environ.get("PORT", "8080")
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1342641151').split()]
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002084798134'))
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
 
 
 # important information for your bot
-S_GROUP = environ.get('S_GROUP', "https://t.me/+1YR5aYuCdr40N2M1")
-S_CHANNEL = environ.get('S_CHANNEL', "https://t.me/amal_nath_05")
+S_GROUP = environ.get('S_GROUP', "")
+S_CHANNEL = environ.get('S_CHANNEL', "")
 
 # for mongodb
-DATABASE_NAME = os.environ.get("DB_NAME", "mrtg")     
-DATABASE_URI  = os.environ.get("DB_URL", "mongodb+srv://mrtg:3rqnL0nfKO1DgVM2@cluster0.m4nrgsu.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = os.environ.get("DB_NAME", "")     
+DATABASE_URI  = os.environ.get("DB_URL", "")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 MONGO_URL = os.environ.get('MONGO_URL', "")
 
 # for openai
-OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY', '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')
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY', '')
 
 #for spotify 
-SPOTIFY_CLIENT_ID = os.environ.get('SPOTIFY_CLIENT_ID', 'd3a0f15a75014999945b5628dca40d0a')
-SPOTIFY_CLIENT_SECRET = os.environ.get('SPOTIFY_CLIENT_SECRET', 'e39d1705e35c47e6a0baf50ff3bb587f')
+SPOTIFY_CLIENT_ID = os.environ.get('SPOTIFY_CLIENT_ID', '')
+SPOTIFY_CLIENT_SECRET = os.environ.get('SPOTIFY_CLIENT_SECRET', '')
