694c82ce0ebbed84afce37f49fe4489bf594661d
MrTG-CodeBot <148360334+MrTG-CodeBot@users.noreply.github.com>
2023-12-19 23:36:06 +0530

    Update song.py

--- plugins/song.py
+++ plugins/song.py
@@ -10,23 +10,31 @@ from youtube_search import YoutubeSearch
 async def download_song(client, message):
     # Check if the user has provided a song name
     if len(message.text.split()) < 2:
-        await message.reply("Please provide the song name you want to download")
+        await message.reply("ᴘʟᴇᴀsᴇ ᴘʀᴏᴠɪᴅᴇ ᴛʜᴇ sᴏɴɢ ɴᴀᴍᴇ ʏᴏᴜ ᴡᴀɴᴛ")
         return
 
-    song_name = message.text.split()[1:]  # Extract the song name from the message
+    song_name = message.text.split()[1:]  # Extract the song name parts into a list
     song_name = " ".join(song_name)  # Combine the song name parts into a single string
+
+    # Send "Searching..." message before searching
+    await message.reply("⏳")
 
     # Search for the song on YouTube
     search_results = YoutubeSearch(song_name, max_results=1).to_dict()
+    if not search_results:
+        await message.reply("ɴᴏ sᴏɴɢ ғᴏᴜɴᴅ ᴡɪᴛʜ ᴛʜᴀᴛ ɴᴀᴍᴇ")
+        return
+
     song_url = search_results[0]["url_suffix"]
     song_title = search_results[0]["title"]
+    duration = search_results[0]["duration"]
 
     # Download the song using pytube
     yt = YouTube(f"https://www.youtube.com{song_url}")
     audio_streams = yt.streams.filter(only_audio=True)
 
     if not audio_streams:
-        await client.send_message(message.chat.id, "No audio stream found for the specified video")
+        await message.reply("ɴᴏ ᴀᴜᴅɪᴏ sᴛʀᴇᴀᴍ ғᴏᴜɴᴅ ғᴏʀ ᴛʜᴇ sᴘᴇᴄɪғɪᴇᴅ ᴠɪᴅᴇᴏ")
         return
 
     video = audio_streams.first()
@@ -34,12 +42,18 @@ async def download_song(client, message):
 
     try:
         video.download(filename=audio_filename)
-        await message.reply(f"**Song downloaded: {audio_filename}**")
 
-        # Send the downloaded song to the user
-        await message.reply_audio(audio_filename)
+        # Prepare message caption with title, duration, and YouTube link
+        caption = f"**🎧 {song_title}**\n" + \
+                    f"🕛 ᴅᴜʀᴀᴛɪᴏɴ: {duration}\n" + \
+                    f"🌿 ʏᴏᴜ ᴛᴜʙᴇ: <a href='https://www.youtube.com{song_url}'>ʏᴏᴜ ᴛᴜʙᴇ</a>"
+
+        # Send downloaded song with caption
+        await message.reply_audio(audio_filename, caption=caption)
 
         # Delete the downloaded song after sending it
         os.remove(audio_filename)
+
     except Exception as e:
-        await message.reply(f"Error downloading song: {e}")
+        await message.reply(f"ᴇʀʀᴏʀ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ sᴏɴɢ: {e}")
+
