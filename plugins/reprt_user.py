22504d8200964702dce9fcd31d642deb0cadf092
MrTG-CodeBot <148360334+MrTG-CodeBot@users.noreply.github.com>
2023-12-29 10:29:13 +0530

    Update reprt_user.py

--- plugins/reprt_user.py
+++ plugins/reprt_user.py
@@ -14,9 +14,8 @@ async def report_user(bot, message):
         success = True
         report = f"𝖱𝖾𝗉𝗈𝗋𝗍𝖾𝗋 : {mention} ({reporter})" + "\n"
         report += f"𝖬𝖾𝗌𝗌𝖺𝗀𝖾 : {message.reply_to_message.link}"
-        # Using latest pyrogram's enums to filter out chat administrators
         async for admin in bot.get_chat_members(chat_id=message.chat.id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
-            if not admin.user.is_bot: # Filtering bots and prevent sending message to bots | Message will be send only to user admins
+            if not admin.user.is_bot: 
                 try:
                     reported_post = await message.reply_to_message.forward(admin.user.id)
                     await reported_post.reply_text(
@@ -27,7 +26,7 @@ async def report_user(bot, message):
                     success = True
                 except:
                     pass
-            else: # Skipping Bots
+            else: 
                 pass
         if success:
             await message.reply_text(script.REPORT_MSG)
