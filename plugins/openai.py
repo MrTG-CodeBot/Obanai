a858f3183cc9626b126534594f312834e32275d9
MrTG-CodeBot <148360334+MrTG-CodeBot@users.noreply.github.com>
2024-01-04 07:20:49 +0530

    Update openai.py

--- plugins/openai.py
+++ plugins/openai.py
@@ -1,27 +1,32 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 import openai
-from info import API_ID, API_HASH, BOT_TOKEN, OPENAI_API_KEY, PORT
+from info import OPENAI_API_KEY, REQUESTED_CHANNEL
 
 openai.api_key = OPENAI_API_KEY
 
 @Client.on_message(filters.command('openai'))
 async def openai_command(client, message):
     if not message.text:
-        await client.send_message(message.chat.id, "Please provide ur request")
+        await client.send_message(message.chat.id, "ᴘʟᴇᴀsᴇ ᴘʀᴏᴠɪᴅᴇ ᴜʀ ʀᴇǫᴜᴇsᴛ ")
         return
 
     try:
         user_input = message.text.split(' ', 1)[1]
 
         response = openai.Completion.create(
-            model="gpt-3.5-turbo-instruct",
-            prompt=user_input,
-            max_tokens=1024
+            model = "text-davinci-003",
+            prompt = user_input,
+            temperature = 0.5,
+            max_tokens = 1000,
+            top_p=1,
+            frequency_penalty=0.1,
+            presence_penalty = 0.0,
         )
-
-        await message.reply_text(response.choices[0].text)
+        ai = response.choices[0].text
+        await message.reply_text(ai)
+        await client.send_message(REQUESTED_CHANNEL, text=f"ᴏᴘᴇɴᴀɪ ʀᴇǫᴜᴇsᴛ ғʀᴏᴍ {message.from_user.mention}\nǫᴜᴇʀʏ ɪs:- {user_input}")
 
     except Exception as e:
-        error_message = f"Sorry, an error occurred: {str(e)}"
+        error_message = f"sᴏʀʀʏ, ᴀɴ ᴇʀʀᴏʀ  ᴏᴄᴄᴜʀᴇᴅ: {str(e)}"
         await message.reply_text(error_message)
