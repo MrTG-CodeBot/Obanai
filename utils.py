c1c92bd0e9952d5628d370117b2a1b60779080db
MrTG-CodeBot <148360334+MrTG-CodeBot@users.noreply.github.com>
2024-01-28 11:34:20 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -62,6 +62,23 @@ async def not_subscribed(_, client, message):
          return False 
    return True
 
+def get_file_id(msg: Message):
+    if msg.media:
+        for message_type in (
+            "photo",
+            "animation",
+            "audio",
+            "document",
+            "video",
+            "video_note",
+            "voice",
+            "sticker"
+        ):
+            obj = getattr(msg, message_type)
+            if obj:
+                setattr(obj, "message_type", message_type)
+                return obj
+
 def get_size(size):
     """Get size in readable format"""
 
