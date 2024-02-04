7406d3a69805d79344c9819ebd4eff723181bcb5
MrTG-CodeBot <148360334+MrTG-CodeBot@users.noreply.github.com>
2024-02-05 03:21:26 +0530

    Create contact_admin.py

---
+++ plugins/contact_admin.py
@@ -0,0 +1,35 @@
+# This is a simple code from MrTG brain 🧠.
+# sending method for users, eg:- /send hi.
+# reply method for admins only, eg:- !ans 2674364 hello 
+# In reply method !ans is command, {user_id} of sended person, {ur_message} your meessage for reply to use
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from info import ADMINS, ADMIN_GROUP_ID
+import asyncio
+
+@Client.on_message(filters.private & filters.command("send"))
+async def forward_message_to_group(client, message):
+ try:
+    text = message.text.split(" ", 1)[1] 
+    user_id = message.from_user.id
+    name = message.from_user.mention
+    await message.forward(ADMIN_GROUP_ID)
+    await client.send_message(ADMIN_GROUP_ID, text=f"ᴀ ɴᴇᴡ ᴍᴇssᴀɢᴇ ғʀᴏᴍ {name}\n\nᴜꜱᴇʀ-ɪᴅ= <code>{user_id}</code>")
+    await client.send_message(ADMIN_GROUP_ID, text=f"ɪғ ʏᴏᴜ ᴡᴀɴɴᴀ ᴛᴏ ʀᴇᴘʟʏ ᴛʜᴇɴ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ\n\n<code>!ans {user_id} ur_messaage</code>")
+    success_message = await message.reply_text("Mᴇssᴀɢᴇ ғᴏʀᴡᴀʀᴅᴇᴅ ᴛᴏ ᴛʜᴇ ᴀᴅᴍɪɴs. ᴡᴀɪᴛ ғᴏʀ ᴛʜᴇ ʀᴇᴘʟʏ.")
+
+ except Exception as e:
+    await message.reply_text(f"error{e}")
+
+@Client.on_message(filters.command("ans", "!") & filters.user(ADMINS) & filters.chat(int(ADMIN_GROUP_ID)))
+async def reply_to_forwarded_message(client, message:Message):
+ try: 
+    mrtg = message.text.split(" ", 2)
+    user_id = int(mrtg[1])
+    reply_text = mrtg[2]
+    await client.send_message(user_id, text=f"Rᴇᴘʟʏ ғʀᴏᴍ ᴍʏ ᴀᴅᴍɪɴ")
+    await client.send_message(user_id, text=f"<code>{reply_text}</code>")
+    await message.reply_text(f"ᴍᴇssᴀɢᴇ sᴇɴᴛ sᴜᴄᴇssғᴜʟʟʏ ᴛᴏ <a href='tg://user?id={user_id}'><b>ᴜsᴇʀ</b></a>")
+ except Exception as e:
+    await message.reply_text(f"error{e}")
