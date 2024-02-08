718a3546c4eaadc3fa1b1e5f39e20708d1441ca4
MrTG-CodeBot <148360334+MrTG-CodeBot@users.noreply.github.com>
2024-02-08 22:50:36 +0530

    Update text_to_qr.py

--- plugins/text_to_qr.py
+++ plugins/text_to_qr.py
@@ -1,10 +1,12 @@
 from pyrogram import Client, filters
 from PIL import Image
 import qrcode
+from info import REQUESTED_CHANNEL
 
 
 @Client.on_message(filters.command("qr"))
 async def generate_qr_code(client, message):
+ try:
     text = message.text.split(maxsplit=1)[1]
     qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
     qr.add_data(text)
@@ -16,3 +18,6 @@ async def generate_qr_code(client, message):
     img.save(img_path)
   
     await message.reply_photo(img_path)
+    await client.send_message(REQUESTED_CHANNEL, text=f"#ǫʀ_ɢᴇɴᴇʀᴀᴛᴏʀ\n\nʀᴇǫᴜᴇsᴛᴇᴅ_ғʀᴏᴍ: {message.from_user.mention}\n ᴛᴇxᴛ: {text}")
+ except Exception as e:
+    await message.reply_text(f"{e}")
